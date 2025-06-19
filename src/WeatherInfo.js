4afa2085388c9d05169e8fb06ae6c9587e3b7a5e
Emma-404 <emmamboronad@gmail.com>
2025-06-19 11:48:59 +0200

    updated forecast
    Forecast on 6 days

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -20,15 +20,17 @@ export default function WeatherInfo(props) {
               <WeatherIcon code={props.data.icon} size={46} />
             </div>
 
-            <WeatherTemperature celsius={props.data.temperature} />
+            <div className="float-left">
+              {Math.round(props.data.temperature)}°
+            </div>
           </div>
         </div>
-      </div>
-      <div className="col-6">
-        <ul>
-          <li>Humidity : {props.data.humidity}%</li>
-          <li>Wind : {props.data.wind}km/h</li>
-        </ul>
+        <div className="col-6">
+          <ul>
+            <li>Humidity : {props.data.humidity}%</li>
+            <li>Wind : {props.data.wind}km/h</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
