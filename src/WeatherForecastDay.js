7d2ef35ad47a7bf58f98bb5767b85b99e26b6767
Emma-404 <emmamboronad@gmail.com>
2025-06-19 11:10:23 +0200

    Debugging

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -3,11 +3,11 @@ import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
-    let temperature = Math.round(props.data.temp.maximum);
+    let temperature = Math.round(props.data.temperature.maximum);
     return `${temperature}°`;
   }
   function minTemperature() {
-    let temperature = Math.round(props.data.temp.mininum);
+    let temperature = Math.round(props.data.temperature.minimum);
     return `${temperature}°`;
   }
 
